fc7b9a65b51bec7bad43e702be876dff1fb18a15
Alaeddine Abdessalem <alaeddine-13@live.fr>
2020-11-29 16:08:28 +0100

    Fix create_super_user and create_user in user model

--- server/taxi/models.py
+++ server/taxi/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager as BuiltinUserManager
 from django.core.validators import RegexValidator
 from phone_verify.models import SMSVerification
 from django.db.models.signals import post_save
@@ -7,7 +7,23 @@ from django.dispatch import receiver
 from django.shortcuts import get_object_or_404
 from .backends.storage_backends import PublicMediaStorage, PrivateMediaStorage
 
-
+class UserManager(BuiltinUserManager):
+    USERNAME_FIELD = 'phone_number'
+    def create_superuser(self, email=None, password=None, **extra_fields):
+        return super().create_superuser(
+            extra_fields.get(self.USERNAME_FIELD),
+            email=email,
+            password=password,
+            **extra_fields
+        )
+    
+    def create_user(self, email=None, password=None, **extra_fields):
+        return super().create_user(
+            extra_fields.get(self.USERNAME_FIELD),
+            email=email,
+            password=password,
+            **extra_fields
+        )
 
 class User(AbstractUser):
     phone_regex = RegexValidator(regex=r'^\+216\d{8}$', message="Phone number must be entered in the format: (+216)xxxxxxxx")
@@ -15,7 +31,8 @@ class User(AbstractUser):
     phone_verified = models.BooleanField(default=False)
     credits = models.IntegerField(default=1000)
     image_url = models.CharField(max_length=255, null=True)
-    USERNAME_FIELD = 'phone_number'
+    objects = UserManager()
+    USERNAME_FIELD = objects.USERNAME_FIELD
 
 @receiver(post_save, sender=SMSVerification)
 def verify_user(sender, instance=None, created=None, **kwargs):
